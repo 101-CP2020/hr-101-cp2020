6d4d1bbf2d0e04f54d5dd78a14178b6e37e23ee3
reynholm <mail@alamra.net>
2020-11-14 22:28:33 +0200

    Заготовка под API

--- app/api/pub/v1/controllers/DefaultController.php
+++ app/api/pub/v1/controllers/DefaultController.php
@@ -2,13 +2,33 @@
 
 namespace app\api\pub\v1\controllers;
 
+use app\api\pub\v1\models\VacancySearchProvider;
+use app\models\Vacancy;
 use Yii;
+use yii\db\Query;
 use yii\rest\Controller;
 
 class DefaultController extends Controller
 {
     public function actionIndex()
     {
-        return "API works!";
+    	$params = \Yii::$app->request->queryParams;
+    	if (!isset($params['month_year']))
+		{
+			$params['month_year'] = $this->getLastDate();
+		}
+
+    	$searchModel = new VacancySearchProvider();
+        $provider = $searchModel->search($params, '');
+
+        return $provider->getModels();
     }
+
+    private function getLastDate()
+	{
+		return (new Query())
+			->select('max(month_year) as date')
+			->from(Vacancy::tableName())
+			->scalar();
+	}
 }
--- app/tables/TblVacancies.php
+++ app/tables/TblVacancies.php
@@ -104,4 +104,10 @@ class TblVacancies extends \yii\db\ActiveRecord
     {
         return $this->hasOne(TblOkved::className(), ['okved' => 'okved']);
     }
+
+    public function getPredictions()
+	{
+		return $this->hasOne(TblPredictions::class, ['okpdtr' => 'okpdtr'])
+			->orderBy([TblPredictions::tableName() . '.created_at' => SORT_DESC])->limit(1);
+	}
 }
